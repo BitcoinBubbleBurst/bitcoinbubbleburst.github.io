a9a391a61daf3c36883fc4fca8c6c9e247fc3a0d
Mr.Robot <hantek.f@gmail.com>
2017-12-02 17:46:58 +0100

    initialize web app

---
+++ app.py
@@ -0,0 +1,33 @@
+#wiki text scrap
+from bs4 import BeautifulSoup
+#html request
+import requests
+from cherrypy.lib import static
+import cherrypy
+import os
+from jinja2 import Environment, FileSystemLoader
+
+localDir = os.path.dirname(__file__)
+absDir = os.path.join(os.getcwd(), localDir)
+
+env = Environment(loader=FileSystemLoader('html'))
+
+class Landing_Page(object):
+    @cherrypy.expose
+    def index(self):
+        tmpl = env.get_template('index.html')
+        return tmpl.render()
+
+config = {
+    'global': {
+        'server.socket_host': '0.0.0.0',
+        'server.socket_port': int(os.environ.get('PORT', 5000)),
+    },
+    '/assets': {
+        'tools.staticdir.root': os.path.dirname(os.path.abspath(__file__)),
+        'tools.staticdir.on': True,
+        'tools.staticdir.dir': 'assets',
+    }
+}
+
+cherrypy.quickstart(Landing_Page(), '/', config=config)
