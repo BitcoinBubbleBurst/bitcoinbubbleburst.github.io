e3b02ae8b1eda05ac67a54211a5e552bd89ca11e
Saad El Hajjaji <selhajjaji@opera.com>
2017-12-03 05:02:21 +0100

    Small fixes

--- js/script.js
+++ js/script.js
@@ -276,10 +276,10 @@
     ========================================================================== */
 
     function toggleIcon(e) {
-    $(e.target)
-        .prev('.panel-heading')
-        .find(".more-less")
-        .toggleClass('glyphicon-plus glyphicon-minus');
+        $(e.target)
+            .prev('.panel-heading')
+            .find(".more-less")
+            .toggleClass('glyphicon-plus glyphicon-minus');
     }
     $('.panel-group').on('hidden.bs.collapse', toggleIcon);
     $('.panel-group').on('shown.bs.collapse', toggleIcon);
